fd0072df7c6f9cd397a7814552243923135b2c41
Jose Sustaita <josesustaita70@gmail.com>
2020-07-22 19:32:11 -0400

    Registration/login
    Server is successfully able to register and login.
    
    Migrations added for users: Username and password are required fields
    
    Seeds were added. Users: "janedoe" and "johndoe" now exisit

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-
+const usersRouter = require("./auth/auth-router");
+const cookieParser = require("cookie-parser");
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(cookieParser());
 
-server.get("/", (req, res) => res.send("Hello World!"));
+server.use(usersRouter);
+server.use((err, req, res, next) => {
+  console.log(err);
 
+  res.status(500).json({
+    message: "Something went wrong",
+  });
+});
 module.exports = server;
